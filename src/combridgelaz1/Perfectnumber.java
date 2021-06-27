3ed8a937214dbb7ccfb58ebafeed7fe891668060
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-27 20:54:31 +0530

    print perfect number

---
+++ src/combridgelaz1/Perfectnumber.java
@@ -0,0 +1,31 @@
+package combridgelaz1;
+import java.util.Scanner;
+public class Perfectnumber {
+
+	public static void main(String[] args) {
+		long n, sum=0;  
+		Scanner sc=new Scanner(System.in);         
+		System.out.print("Enter the number: ");  
+		n=sc.nextLong();  
+		int i=1;   
+		while(i <= n/2)  
+		{  
+		if(n % i == 0)  
+		{   
+		sum = sum + i;
+		System.out.println(sum);
+		} 
+		i++;  
+		}  
+		  
+		if(sum==n)  
+		{  	
+		System.out.println(n+" is a perfect number.");
+		
+		} 
+		else  	 
+		System.out.println(n+" is not a perfect number.");   
+		} 
+	}
+
+
