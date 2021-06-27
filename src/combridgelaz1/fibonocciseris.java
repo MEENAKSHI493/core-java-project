aad5826dbac27fa6cad1c9984be15288ea15e034
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-27 20:37:06 +0530

    print fibonocci series

--- src/combridgelaz1/fibonocciseris.java
+++ src/combridgelaz1/fibonocciseris.java
@@ -1,9 +1,45 @@
 package combridgelaz1;
 
+import java.util.Scanner;
+
+import com.Bridgelabz.fibonocciseries;
+
 public class fibonocciseris {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		static int n1 = 0, n2 = 1, n3 = 0;
+		
+
+		public static void main(String[] args) {
+					
+			fibonocciseries fib=new fibonocciseries();
+			fib.printFibonacci();
+			
+
+		}
+
+		 void printFibonacci() {
+		   Scanner sc= new Scanner(System.in); //System.in is a standard input stream.
+		   int previousnumber = -1;
+		   int nextnumber = 1;
+		   int sum = 0;
+		   System.out.print("Enter first number- ");
+		   int a= sc.nextInt();
+				
+			for(int i=0;i<a;i++)
+			{
+			  sum=previousnumber+nextnumber;
+			  System.out.println(sum);
+			  previousnumber=nextnumber;
+			  nextnumber=sum;		 
+			}
+		    printFibonacci();
+		   }
+			
+
+		}
+
+
 
 	}
 
